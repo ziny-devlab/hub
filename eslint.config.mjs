57a080eb270d658df5587f6e9e0e70f522357a3b
zinny22 <hziny722@naver.com>
2025-08-06 12:52:36 +0900

    fix: eslint rule 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'react-hooks/exhaustive-deps': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }
 ];
 
 export default eslintConfig;
