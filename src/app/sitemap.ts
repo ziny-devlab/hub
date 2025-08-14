82cd12d74a9e29d7fb7c7fb8593dff3882324fa9
zinny22 <hziny722@naver.com>
2025-08-14 13:45:27 +0900

    fix: 사이트맵 관련 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,17 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 1,
     },
-    {
-      url: `https://auto-docs2.vercel.app/landing`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `https://doc-summary-gold.vercel.app/`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    }
   ]
 }
